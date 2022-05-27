db214f399d00f376eb472240362728b1c4837775
NAVENDRA SINGH SHEKHAWAT <mackjmis@gmail.com>
2022-05-27 10:49:22 +0530

    Linear Layout Explained

---
+++ DemoLinearLayout/app/src/main/java/com/example/demolinearlayout/DemoLinearLayout2.java
@@ -0,0 +1,14 @@
+package com.example.demolinearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DemoLinearLayout2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_linear_layout2);
+    }
+}
