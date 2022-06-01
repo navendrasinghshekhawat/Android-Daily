497620589224045cd1b6d4c68d03a162284bcb62
NAVENDRA SINGH SHEKHAWAT <mackjmis@gmail.com>
2022-06-01 09:18:26 +0530

    Added Scroll Bar

---
+++ Demo_Scroll/app/src/main/java/com/example/demo_scroll/DemoIntent.java
@@ -0,0 +1,57 @@
+package com.example.demo_scroll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+//this is a program for demo of how intent works
+public class DemoIntent extends AppCompatActivity {
+
+    Button b1,b2,b3,b4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_intent);
+
+        b1=findViewById(R.id.button1);
+        b2=findViewById(R.id.button2);
+        b3=findViewById(R.id.button3);
+        b4=findViewById(R.id.button4);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(DemoIntent.this,DemoHorizontalScrollBar.class);
+                startActivity(i);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ratindia.com/"));
+                startActivity(i);
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(Intent.ACTION_DIAL, Uri.parse("tel:9351467408"));
+                startActivity(i);
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(Intent.ACTION_DIAL, Uri.parse("tel:9351467408"));
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ Demo_Scroll/app/src/main/java/com/example/demo_scroll/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.demo_scroll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//this is a program for vertical scroll bars
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
